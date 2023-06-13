f246d479ca1cd2ed5f7c71e496e9b4af8a94836d
cupcaaakes <74048222+cupcaaakes@users.noreply.github.com>
2023-06-13 12:01:24 +0200

    MPU init implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,212 @@
 #include <Arduino.h>
-/**
-// put function declarations here:
-#define RXBluetooth = DD3
-#define TXBluetooth = DD2
-#define ClockMPU = SCL
-#define DataMPU = SDA
+#include "GY521.h"
 
+GY521 sensor(0x68);
+
+uint32_t counter = 0;
+
+float ax, max, ay, az;
+float gx, gy, gz;
+float t;
+
+
+
+// put function declarations here:
+#define RXBluetooth  DD3
+#define TXBluetooth  DD2
+#define ClockMPU  SCL
+#define DataMPU  SDA
+
+#define  DirRight  1
+#define  DirLeft  2
+#define DirUp  3
+#define DirDown 4
+#define bar 0.75
+
+int buttonPressed;
 int myFunction(int, int);
+float correction;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  Serial.println("Initializing Connection");
+  sensor.reset();
+  delay(10);
+  sensor.begin();
+  sensor.read();
+  correction = -sensor.getAccelX(); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //sensor.reset();
+  sensor.read();
+
+  if(sensor.getAccelX() + correction > bar){
+    buttonPressed = DirDown;
+  }else   if(sensor.getAccelX() +correction < -bar){
+    buttonPressed = DirUp;
+  }else   if(sensor.getAccelY() > bar){
+    buttonPressed = DirLeft;
+  }else   if(sensor.getAccelY() < -bar){
+    buttonPressed = DirRight;
+  }else if (sensor.getAccelZ() > bar || sensor.getAccelZ() < -bar){
+    buttonPressed= 0;
+  }
+  Serial.print( correction);
+  Serial.print(" ");
+  Serial.print( sensor.getAccelX());
+  Serial.print(" ");
+  Serial.print( sensor.getAccelY());
+  Serial.print(" ");
+  Serial.println(buttonPressed);
+
+  /*
+  if(tempX > ax){
+  ax = tempX;
+  }
+  if (tempX < max){
+    max = tempX;
+  }
+  //ay = sensor.getAccelY();  az = sensor.getAccelZ();
+  Serial.print(ax);
+  Serial.print(" ");
+  Serial.print(max); //Serial.print(" "); Serial.print(ay); Serial.print(" "); Serial.print(az);
+  Serial.println();
+  */
+  delay(25);
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
 }
+
+/*
+#include "GY521.h"
+
+GY521 sensor(0x68);
+
+uint32_t counter = 0;
+
+float ax, ay, az;
+float gx, gy, gz;
+float t;
+
+
+void setup()
+{
+  Serial.begin(9600
+);
+  Serial.println();
+  Serial.println(__FILE__);
+  Serial.print("GY521_LIB_VERSION: ");
+  Serial.println(GY521_LIB_VERSION);
+
+  Wire.begin();
+  delay(100);
+  if (sensor.wakeup() == false)
+  {
+    Serial.println("Could not connect to GY521");
+  }
+  //  adjust when needed.
+  sensor.setAccelSensitivity(0);  //  2g
+  sensor.setGyroSensitivity(0);   //  250 degrees/s
+  sensor.setThrottle(false);
+
+  //  set all calibration errors to zero
+  sensor.axe = 0;
+  sensor.aye = 0;
+  sensor.aze = 0;
+  sensor.gxe = 0;
+  sensor.gye = 0;
+  sensor.gze = 0;
+
+  Serial.println("\n\nReading calibration numbers...");
+}
+
+
+void loop()
+{
+  ax = ay = az = 0;
+  gx = gy = gz = 0;
+  t = 0;
+  for (int i = 0; i < 100; i++)
+  {
+    sensor.read();
+    ax -= sensor.getAccelX();
+    ay -= sensor.getAccelY();
+    az -= sensor.getAccelZ();
+    gx -= sensor.getGyroX();
+    gy -= sensor.getGyroY();
+    gz -= sensor.getGyroZ();
+    t += sensor.getTemperature();
+  }
+
+  if (counter % 10 == 0)
+  {
+    Serial.println("\n\tCOPY CODE SNIPPET");
+    Serial.print("sensor.axe = ");
+    Serial.print(sensor.axe, 7);
+    Serial.print(";\n");
+
+    Serial.print("sensor.aye = ");
+    Serial.print(sensor.aye, 7);
+    Serial.print(";\n");
+
+    Serial.print("sensor.aze = ");
+    Serial.print(sensor.aze, 7);
+    Serial.print(";\n");
+    
+    Serial.print("sensor.gxe = ");
+    Serial.print(sensor.gxe, 7);
+    Serial.print(";\n");
+
+    Serial.print("sensor.gye = ");
+    Serial.print(sensor.gye, 7);
+    Serial.print(";\n");
+
+    Serial.print("sensor.aze = ");
+    Serial.print(sensor.gze, 7);
+    Serial.print(";\n");
+
+    Serial.println("\taxe\taye\taze\tgxe\tgye\tgze\tT");
+  }
+
+  if (counter % 10 == 0)
+  {
+  Serial.print(counter);
+  Serial.print('\t');
+  Serial.print(ax * 0.01, 3);
+  Serial.print('\t');
+  Serial.print(ay * 0.01, 3);
+  Serial.print('\t');
+  Serial.print(az * 0.01, 3);
+  Serial.print('\t');
+  Serial.print(gx * 0.01, 3);
+  Serial.print('\t');
+  Serial.print(gy * 0.01, 3);
+  Serial.print('\t');
+  Serial.print(gz * 0.01, 3);
+  Serial.print('\t');
+  Serial.print(t * 0.01, 2);
+  Serial.println();
+  }
+  //  adjust calibration errors so table should get all zero's.
+  sensor.axe += ax * 0.01;
+  sensor.aye += ay * 0.01;
+  sensor.aze += az * 0.01;
+  sensor.gxe += gx * 0.01;
+  sensor.gye += gy * 0.01;
+  sensor.gze += gz * 0.01;
+
+  counter++;
+
+}
+
 */
+/*
 #include "Wire.h" // This library allows you to communicate with I2C devices.
 
 const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
@@ -73,3 +258,5 @@ void loop() {
   
   // delay
   delay(100);}
+  
+*/
